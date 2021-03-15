4505e6d402893427e8691ed967d5bf744c66b5af
Andrea Suarez <andreasuarez@MacBook-Pro-de-Andrea-2.local>
2021-03-15 20:08:12 -0300

    if promise fails on all promise, all fail

--- promise-api.js
+++ promise-api.js
@@ -4,9 +4,10 @@
 // const q = Promise.reject(new Error ('Reason for rejection...'));
 // p.catch(err => console.log(err));
 
-const p1 = new Promise((resolve) => {
+const p1 = new Promise((resolve,reject) => {
     setTimeout(() => {
         console.log('Parallel Promise 1....');
+        //reject(new Error('Something went wrong...'))
         resolve(1);
     },2000)
 });
@@ -18,4 +19,8 @@ const p2 = new Promise((resolve) => {
     },2000)
 });
 Promise.all([p1,p2])
-    .then(result => console.log(result))
+    .then(result => console.log(result));   
+    //.catch(err => console.log ('Error', err.message))
+
+
+// if one of the two promises its rejected,, the array of all promises with be rejected too.
