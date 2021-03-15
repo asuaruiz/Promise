c28011198c69bdfff404eb8fc865269be3a5bb4f
Andrea Suarez <andreasuarez@MacBook-Pro-de-Andrea-2.local>
2021-03-15 19:41:11 -0300

    Changing callback for promise in first two callbacks

--- index.js
+++ index.js
@@ -8,18 +8,25 @@ getUser(1, (user) => {
 });
 console.log('After');
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'mosh' });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve,reject) => {
+    //Async To simulate Promise
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  })
+  
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise ((resolve,reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  })
+  
 }
 
 function getCommits(repo, callback) {
