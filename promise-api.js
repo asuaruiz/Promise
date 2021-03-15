fdcba0103426eec7a8acfff8ba531c55bce51bf6
Andrea Suarez <andreasuarez@MacBook-Pro-de-Andrea-2.local>
2021-03-15 20:03:36 -0300

    parallel promise testing

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,21 @@
-const p = Promise.resolve({id:1});
-p.then(result => console.log(result));
+// const p = Promise.resolve({id:1});
+// p.then(result => console.log(result));
 
-const q = Promise.reject(new Error ('Reason for rejection...'));
-p.catch(err => console.log(err));
+// const q = Promise.reject(new Error ('Reason for rejection...'));
+// p.catch(err => console.log(err));
+
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Parallel Promise 1....');
+        resolve(1);
+    },2000)
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Parallel Promise 2....');
+        resolve(2);
+    },2000)
+});
+Promise.all([p1,p2])
+    .then(result => console.log(result))
